41ee12a1a7f8f8a16c321def4f346cbab22bd875
ac困难户 <746445568@qq.com>
2026-03-29 22:04:06 +0800

    chore: 删除废弃的 WinUI 桌面端

--- apps/desktop/OJReviewDesktop/Imports.cs
+++
@@ -1,7 +0,0 @@
-global using OJReviewDesktop.Views;
-global using OJReviewDesktop.Views.Pages;
-global using OJReviewDesktop.Models;
-global using OJReviewDesktop.Services;
-
-global using Microsoft.UI.Xaml;
-global using Microsoft.UI.Xaml.Controls;
